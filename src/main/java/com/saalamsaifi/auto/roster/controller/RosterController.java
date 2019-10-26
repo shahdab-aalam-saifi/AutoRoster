8d4ce068b20bf702dfe5490c320be144c3dbc12d
Shahdab Aalam Saifi <saalamsaifi@gmail.com>
2019-10-26 20:54:24 +0530

    Excel style changes

--- src/main/java/com/saalamsaifi/auto/roster/controller/RosterController.java
+++ src/main/java/com/saalamsaifi/auto/roster/controller/RosterController.java
@@ -32,7 +32,8 @@ import com.saalamsaifi.auto.roster.service.ExportService;
 
 @RestController
 public class RosterController {
-	private static final String TEAM = "{\"name\":\"Operational Intelligence\",\"maxWfrlAllowed\":2,\"groups\":[{\"name\":\"SECLib\",\"maxWfrlAllowed\":2,\"members\":[{\"name\":\"Sheela Naik\",\"isInterested\":true,\"likes\":[],\"dislikes\":[]},{\"name\":\"Shahdab Aalam Saifi\",\"isInterested\":false,\"likes\":[],\"dislikes\":[]},{\"name\":\"Kashif Jilani\",\"isInterested\":true,\"likes\":[],\"dislikes\":[]},{\"name\":\"Rahul Vageriya\",\"isInterested\":true,\"likes\":[],\"dislikes\":[]},{\"name\":\"Santosh Nayak\",\"isInterested\":true,\"likes\":[],\"dislikes\":[]}]},{\"name\":\"GIG\",\"maxWfrlAllowed\":1,\"members\":[{\"name\":\"Pankaj Bhandari\",\"isInterested\":true,\"likes\":[],\"dislikes\":[]},{\"name\":\"Priyank Sharma\",\"isInterested\":true,\"likes\":[],\"dislikes\":[]}]},{\"name\":\"APIX\",\"maxWfrlAllowed\":1,\"members\":[{\"name\":\"Robin Phukan\",\"isInterested\":true,\"likes\":[],\"dislikes\":[]},{\"name\":\"Rajnikant Vanpratiwar\",\"isInterested\":true,\"likes\":[],\"dislikes\":[]}]},{\"name\":\"Platform\",\"maxWfrlAllowed\":3,\"members\":[{\"name\":\"Dhiraj DParshionikar\",\"isInterested\":true,\"likes\":[],\"dislikes\":[]},{\"name\":\"Mishika Srivastava\",\"isInterested\":true,\"likes\":[],\"dislikes\":[]},{\"name\":\"Kunjan Dagli\",\"isInterested\":true,\"likes\":[],\"dislikes\":[]},{\"name\":\"Yogesh Pathak\",\"isInterested\":true,\"likes\":[],\"dislikes\":[]},{\"name\":\"Akshay Pulugurtha\",\"isInterested\":true,\"likes\":[],\"dislikes\":[]},{\"name\":\"Saipada Senapati\",\"isInterested\":true,\"likes\":[],\"dislikes\":[]},{\"name\":\"Ibrahim Shaikh\",\"isInterested\":true,\"likes\":[],\"dislikes\":[]},{\"name\":\"Amol Bathe\",\"isInterested\":true,\"likes\":[],\"dislikes\":[]},{\"name\":\"Abhisekh Kumar\",\"isInterested\":false,\"likes\":[],\"dislikes\":[]},{\"name\":\"Sandip Gharate\",\"isInterested\":false,\"likes\":[],\"dislikes\":[]}]},{\"name\":\"Start\",\"maxWfrlAllowed\":1,\"members\":[{\"name\":\"Shirinivas Karlekar\",\"isInterested\":true,\"likes\":[],\"dislikes\":[]},{\"name\":\"Rahul Bhagat\",\"isInterested\":true,\"likes\":[],\"dislikes\":[]}]},{\"name\":\"GNE\",\"maxWfrlAllowed\":1,\"members\":[{\"name\":\"Kedar Joshi\",\"isInterested\":true,\"likes\":[],\"dislikes\":[]},{\"name\":\"Pradeep Patil\",\"isInterested\":true,\"likes\":[],\"dislikes\":[]},{\"name\":\"Anish Nama\",\"isInterested\":true,\"likes\":[],\"dislikes\":[]}]},{\"name\":\"Message+\",\"maxWfrlAllowed\":1,\"members\":[{\"name\":\"Dhananjay Karnataki\",\"isInterested\":true,\"likes\":[],\"dislikes\":[]},{\"name\":\"Chinmay Jog\",\"isInterested\":true,\"likes\":[],\"dislikes\":[]}]},{\"name\":\"CIOT\",\"maxWfrlAllowed\":1,\"members\":[{\"name\":\"Prasanna Huggi\",\"isInterested\":true,\"likes\":[],\"dislikes\":[]},{\"name\":\"Premjeet Kumar\",\"isInterested\":true,\"likes\":[],\"dislikes\":[]}]},{\"name\":\"Data Product\",\"maxWfrlAllowed\":2,\"members\":[{\"name\":\"Biswajit Panigrahi\",\"isInterested\":true,\"likes\":[],\"dislikes\":[]},{\"name\":\"Sanchin Jankar\",\"isInterested\":true,\"likes\":[],\"dislikes\":[]},{\"name\":\"Vishal Narkhede\",\"isInterested\":true,\"likes\":[],\"dislikes\":[]},{\"name\":\"Suchit Nasre\",\"isInterested\":true,\"likes\":[],\"dislikes\":[]},{\"name\":\"Sanjog Mehta\",\"isInterested\":true,\"likes\":[],\"dislikes\":[]},{\"name\":\"Rahul Mane\",\"isInterested\":true,\"likes\":[],\"dislikes\":[]}]},{\"name\":\"Chatbot\",\"maxWfrlAllowed\":1,\"members\":[{\"name\":\"Nandini Kalita\",\"isInterested\":true,\"likes\":[],\"dislikes\":[]},{\"name\":\"Kaustav Banerjee\",\"isInterested\":true,\"likes\":[],\"dislikes\":[]},{\"name\":\"Akhsay Udhane\",\"isInterested\":true,\"likes\":[],\"dislikes\":[]}]}]}";
+	private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+	private static final DateTimeFormatter FILE_DATE_FORMAT = DateTimeFormatter.ofPattern("dd_MM_yyyy");
 
 	@Autowired
 	private ExportService exportService;
@@ -49,13 +50,14 @@ public class RosterController {
 		return target;
 	}
 
-	@PostMapping(path = { URL_QUICK_ROSTER }, produces = { MediaType.APPLICATION_JSON_VALUE }, params = { START_DATE, END_DATE })
+	@PostMapping(path = { URL_QUICK_ROSTER }, produces = { MediaType.APPLICATION_JSON_VALUE }, params = { START_DATE,
+			END_DATE })
 	public void generateRoster(@RequestBody @Valid Team team,
 			@RequestParam(required = true, name = START_DATE) String strStartDate,
 			@RequestParam(required = true, name = END_DATE) String strEndDate) {
-		LocalDate startDate = LocalDate.parse(strStartDate, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-		LocalDate endDate = LocalDate.parse(strEndDate, DateTimeFormatter.ofPattern("dd/MM/yyyy")).plusDays(1);
-		
+		LocalDate startDate = LocalDate.parse(strStartDate, DATE_FORMAT);
+		LocalDate endDate = LocalDate.parse(strEndDate, DATE_FORMAT).plusDays(1);
+
 		WfrlAllocation wfrlAllocation = new WfrlAllocation(team);
 
 		Table<String, String, String> table = TreeBasedTable.create();
@@ -81,8 +83,7 @@ public class RosterController {
 			temp = temp.plusDays(1);
 		}
 
-		exportService.export(table,
-				team.getName() + "_" + startDate.format(DateTimeFormatter.ofPattern("dd_MM_yyyy")) + ".xlsx");
+		exportService.export(table, team.getName() + "_" + startDate.format(FILE_DATE_FORMAT) + ".xlsx");
 
 	}
 }
--- src/main/java/com/saalamsaifi/auto/roster/service/impl/ExportToExcelServiceImpl.java
+++ src/main/java/com/saalamsaifi/auto/roster/service/impl/ExportToExcelServiceImpl.java
@@ -5,11 +5,17 @@ import static com.saalamsaifi.auto.roster.constant.ProjectConstant.SEPARATOR;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Set;
-
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.VerticalAlignment;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.util.CellRangeAddress;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
@@ -22,6 +28,9 @@ public class ExportToExcelServiceImpl implements ExportService {
 	private static final String TEAM = "Team";
 	private static final String NAME = "Name";
 
+	/**
+	 *
+	 */
 	@Override
 	public void export(Table<String, String, String> table, String fileName) {
 		Set<String> rowKeys = table.rowKeySet();
@@ -53,7 +62,15 @@ public class ExportToExcelServiceImpl implements ExportService {
 				}
 			}
 
-			autoSizeColumn(sheet, colKeys.size() + 2);
+			int columns = colKeys.size() + 2;
+			int rows = rowKeys.size();
+
+			// Formating of output
+			autoSizeColumn(sheet, columns);
+			mergeCell(sheet, rows, 0);
+			styleRow(sheet, 0, columns);
+
+			// Write output to file
 			write(workbook, fileName);
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -61,6 +78,11 @@ public class ExportToExcelServiceImpl implements ExportService {
 
 	}
 
+	/**
+	 * @param row
+	 * @param colNumber
+	 * @param value
+	 */
 	private void setCellValue(final Row row, int colNumber, String value) {
 		Cell cell = row.createCell(colNumber);
 		if (!StringUtils.isEmpty(value)) {
@@ -68,6 +90,10 @@ public class ExportToExcelServiceImpl implements ExportService {
 		}
 	}
 
+	/**
+	 * @param workbook
+	 * @param fileName
+	 */
 	private void write(final Workbook workbook, String fileName) {
 		try (FileOutputStream stream = new FileOutputStream(fileName)) {
 			workbook.write(stream);
@@ -76,9 +102,60 @@ public class ExportToExcelServiceImpl implements ExportService {
 		}
 	}
 
+	/**
+	 * @param sheet
+	 * @param size
+	 */
 	private void autoSizeColumn(final Sheet sheet, int size) {
 		for (int i = 0; i < size; i++) {
 			sheet.autoSizeColumn(i);
+		}
+	}
+
+	/**
+	 * @param sheet
+	 * @param rows
+	 * @param column
+	 */
+	private void mergeCell(final Sheet sheet, int rows, int column) {
+		int i = 0;
+
+		CellStyle style = sheet.getWorkbook().createCellStyle();
+		style.setAlignment(HorizontalAlignment.CENTER);
+		style.setVerticalAlignment(VerticalAlignment.CENTER);
+
+		while (i <= rows) {
+			Cell cell = sheet.getRow(i).getCell(column);
+			while (i + 1 <= rows
+					&& cell.getStringCellValue().equals(sheet.getRow(i + 1).getCell(column).getStringCellValue())) {
+				i++;
+			}
+			if (i - cell.getRowIndex() > 0) {
+				sheet.addMergedRegion(new CellRangeAddress(cell.getRowIndex(), i, column, column));
+				cell.setCellStyle(style);
+			}
+			i++;
+		}
+	}
+
+	/**
+	 * @param row
+	 */
+	private void styleRow(final Sheet sheet, int row, int columns) {
+		Workbook workbook = sheet.getWorkbook();
+
+		Font font = workbook.createFont();
+		font.setBold(true);
+
+		CellStyle style = workbook.createCellStyle();
+		style.setFillForegroundColor(IndexedColors.YELLOW.getIndex());
+		style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+		style.setAlignment(HorizontalAlignment.CENTER);
+		style.setVerticalAlignment(VerticalAlignment.CENTER);
+		style.setFont(font);
+
+		for (int i = 0; i < columns; i++) {
+			sheet.getRow(row).getCell(i).setCellStyle(style);
 		}
 	}
 
